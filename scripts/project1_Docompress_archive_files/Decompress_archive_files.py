638ab068d71854b765fd53ee591fe6cecdc9642e
Zakarya <kakaa2993@gmail.com>
2021-05-11 08:13:21 +0100

    Organise the project and move the Decomprss_archive_files.py to the directory project1_Decompress_archive_files

---
+++ scripts/project1_Docompress_archive_files/Decompress_archive_files.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import os
+import  patoolib
+
+patool_Supported_archive_formats =['.7z','.ace','.adf','.alz','.ape','.a','.arc','.arj','.bz2','.cab','.Z','.cpio','.deb', '.dms', '.flac','.gz', '.iso','.lrz','.lha', '.lzh','.lz', '.lzma','.lzo','.rpm','.rar','.rz','.shn','.tar','.xz','.zip', '.jar','.zoo' ,'.zpaq']
+
+for file in os.listdir('.'):
+    if '.' in file:                    #check if the file is an archive or dirictory
+    	for archive_format in patool_Supported_archive_formats:
+    		if archive_format in file:
+	            print(file)
+	            index = file.index('.')
+	            file_name = file[:index]
+	            file_dir = os.getcwd()+'/'+file_name
+	            print("======>",file_name)
+	            try:        #create the folder and extract the files
+	                    if file_name not in os.listdir('.'):
+	                            os.mkdir(file_name)
+	                    print("*****>",file_name)
+	                    try:
+	                            patoolib.extract_archive(file,outdir=file_dir)
+	                    except:
+	                            print("Unsupported archive format")
+	                            pass
+	            except:      #the folder exist, extract the files
+	                    print("="*40,'The folder exists',"="*40)
+
+	                    print("*****>",file_name)
+	                    patoolib.extract_archive(file,outdir=file_dir)
+	            else:         #the folder and the files exist,exit
+	                    print("="*40,'The Unpack files exist'"="*40,)
+	            print(file," Umpacked Succcessfully")
+
+
