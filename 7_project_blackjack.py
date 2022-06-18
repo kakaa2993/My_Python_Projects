1aa7c0863f0ea57e8566d4c660fd8d146b3986a1
kakaa2993@ <kakaa2993@gmail.com>
2022-06-18 23:04:49 +0100

    add the comparition between the DEALER and the PLAYER

--- 7_project_blackjack.py
+++ 7_project_blackjack.py
@@ -151,7 +151,16 @@ def main(Money=5000):
 
 
 			# Do the comparision between the DEALER and the PLAYER
-			
+			if abs(PLAYER - 21) > abs(DEALER - 21):
+				print("Bust")
+                                Money = Money -  (Bet*2)
+                                print(" Money : {} ".format(Money))
+                                main(Money)
+			else:
+				print("You win $ {} !!!".format(Bet*2))
+                                Money = Money + (Bet*2)
+                                print("Your Money = {}".format(Money))
+                                main(Money) 
 
 
 
