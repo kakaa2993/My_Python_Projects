f812fd7ebec35bd693282bcc29fa8470734ac7b5
kakaa2993@ <kakaa2993@gmail.com>
2022-07-17 09:01:40 -0700

    add check the player typo

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -43,51 +43,50 @@ scissors = '''
 '''
 
 player = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors.\n"))
-choices = [rock, paper, scissors]
-computer_choice = random.choice(choices)
 
-# print the player ASCII
-if player == 0:
-	print(rock)
-elif player == 1:
-	print(paper)
-elif player == 2:
-	print(scissors)
+if player in [0, 1 ,2]:
+    choices = [rock, paper, scissors]
+    computer_choice = random.choice(choices)
+
+    # print the player ASCII
+    player_ASCII =[rock,paper, scissors]
+    print(player_ASCII[player])
+
+
+    #print the computer ASCII
+    print("Computer chose:")
+    print(computer_choice)
+
+
+    # -1 : means lose
+    #  0 : means equal
+    #  1 : means win
+
+    #         Rock Paper Scissors
+    #Rock       0    -1      1
+    #Paper      1     0     -1
+    #Scissors  -1     1      0 
+
+
+    computer_rock = [ 0,-1, 1]
+    computer_paper = [ 1, 0,-1]
+    computer_scissors = [-1, 1, 0]
+
+    # print the result 
+    result = 0
+    if computer_choice == rock:
+        result = computer_rock[int(player)]
+    elif computer_choice == scissors:
+        result = computer_scissors[int(player)]
+    elif computer_choice == paper:
+        result = computer_paper[int(player)]
+
+
+    if result == 0:
+        print("Equal")
+    elif result == -1:
+        print("You win!")
+    else:
+        print("You lose!")
 else:
-	print('You are wrong !')
-
-#print the computer ASCII
-print("Computer chose:")
-print(computer_choice)
-
-
-# -1 : means lose
-#  0 : means equal
-#  1 : means win
-
-#         Rock Paper Scissors
-#Rock       0    -1      1
-#Paper      1     0     -1
-#Scissors  -1     1      0 
-
-
-computer_rock = [ 0,-1, 1]
-computer_paper = [ 1, 0,-1]
-computer_scissors = [-1, 1, 0]
-
-# print the result 
-result = 0
-if computer_choice == rock:
-	result = computer_rock[int(player)]
-elif computer_choice == scissors:
-	result = computer_scissors[int(player)]
-elif computer_choice == paper:
-	result = computer_paper[int(player)]
-
-
-if result == 0:
-    print("Equal")
-elif result == -1:
-    print("You win!")
-else:
-    print("You lose!")
+    print("Invalid typo, please you can only paly with '0', or '1' or '2'.")
