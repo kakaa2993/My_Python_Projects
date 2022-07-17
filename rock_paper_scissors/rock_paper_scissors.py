b4263b5a502135a89608bd04334203a06971614d
kakaa2993@ <kakaa2993@gmail.com>
2022-07-17 07:55:43 -0700

    add the functionality to the script

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 
 '''A rock, paper, scissors game.'''
+'''
+The Rules:
+The outcome of the game is determined by 3 simple rules: 
+ Rock wins against scissors.
+ Scissors win against paper.
+ Paper wins against rock.
 
+'''
+import random
+import time
 
+random.seed(time.time())
 
 rock = '''
     _______
@@ -30,4 +40,41 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-print(rock)
+
+player = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors.\n"))
+choices = [rock, paper, scissors]
+computer_choice = random.choice(choices)
+
+if player == 0:
+	print(rock)
+elif player == 1:
+	print(paper)
+elif player == 2:
+	print(scissors)
+else:
+	print('You are wrong !')
+
+print("Computer chose:")
+print(computer_choice)
+
+#
+computer_rock = [ 0,-1, 1]
+computer_paper = [ 1, 0,-1]
+computer_scissors = [-1, 1, 0]
+
+
+result = 0
+if computer_choice == rock:
+	result = computer_rock[int(player)]
+elif computer_choice == scissors:
+	result = computer_scissors[int(player)]
+elif computer_choice == paper:
+	result = computer_paper[int(player)]
+
+
+if result == 0:
+    print("Equal")
+elif result == -1:
+    print("You win!")
+else:
+    print("You lose!")
