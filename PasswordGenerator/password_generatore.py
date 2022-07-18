1787a9df0773168b0bc2513922ae0390a28ed6ff
kakaa2993@ <kakaa2993@gmail.com>
2022-07-18 02:41:48 -0700

    write the generation func

--- PasswordGenerator/password_generatore.py
+++ PasswordGenerator/password_generatore.py
@@ -8,6 +8,25 @@ for num in range(ord("A"),ord("z")):
 numbers = [num for num in range(10)]
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+',"\\"]
 #symobls = [ _ for _ in '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~']
-#print(symbols)
 
+# Display the welcome message
 
+print("Welcome to the Password Generator!")
+
+num_of_letters = int(input("How many letters would you like in your password?\n"))
+num_of_symbols = int(input("How many symbols would you like?\n"))
+num_of_numbers = int(input('How many numbers would you like?\n'))
+
+password = []
+index = 0
+for _ in range(num_of_letters):
+	index = random.choice(letters)
+	password.append(letters[index])
+for _ in range(num_of_symbols):
+	index = random.choice(letters)
+	password.append(symbols[index])
+for _ in range(num_of_lnumbers):
+	index = random.choice(letters)
+	password.append(numbers[index])
+
+print(password)
