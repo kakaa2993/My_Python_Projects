5e091fbf745485f07f4fe87b0775ce5140f40cc5
kakaa2993@ <kakaa2993@gmail.com>
2022-07-18 03:57:57 -0700

    add more symbols to the password generator

--- PasswordGenerator/password_generatore.py
+++ PasswordGenerator/password_generatore.py
@@ -26,8 +26,8 @@ for num in range(ord("A"),ord("z")):
 	if chr(num) not in ['[', '\\', ']', '^', '_', '`',]:
 		letters.append(chr(num))
 numbers = [num for num in range(10)]
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-#symobls = [ _ for _ in '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~']
+#symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+symbols = [ _ for _ in '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~']
 
 # Display the welcome message
 
