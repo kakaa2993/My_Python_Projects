531f5af61a492fae17d6c98e8ea20bd22ac06f89
kakaa2993@ <kakaa2993@gmail.com>
2022-07-21 13:36:50 -0700

    add a logo and and the option to play again and and the functionality to allwo the player to chose  as he want to as a shift number

--- Caesar_Cipher/main.py
+++ Caesar_Cipher/main.py
@@ -1,28 +1,48 @@
 #!/usr/bin/python3
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']#, #" "]
+from art import logo
+import os
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-len_alpha = len(alphabet)
-def caesar(plain_text, shift_amount, direction):
-    result_text = ""
-    # if (direction != "encode") or (direction != "decode"):
-    #     print("You can only type (encode) for encrypting text and (decode) for decoding text")
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        if direction == "decode":
-            new_position = position- shift_amount
-            result_text += alphabet[new_position]
-        if direction == "encode":
-            new_position = (position + shift_amount) - len_alpha
-            if position + shift_amount > len_alpha-1:
-                result_text += alphabet[new_position]
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#clear the screen
+os.system("clear")
+print(logo)
+end_of_game = False
+
+#the main loop
+while not end_of_game:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    len_alpha = len(alphabet)
+    def caesar(plain_text, shift_amount, direction):
+        result_text = ""
+        # if (direction != "encode") or (direction != "decode"):
+        #     print("You can only type (encode) for encrypting text and (decode) for decoding text")
+
+        shift_amount = shift_amount % 26
+        for letter in plain_text:
+            if letter not in alphabet:
+                result_text += letter
             else:
-                result_text += alphabet[position + shift_amount]
+                position = alphabet.index(letter)
+                if direction == "decode":
+                    new_position = position- shift_amount
+                    result_text += alphabet[new_position]
+                if direction == "encode":
+                    new_position = (position + shift_amount) - len_alpha
+                    if position + shift_amount > len_alpha-1:
+                        result_text += alphabet[new_position]
+                    else:
+                        result_text += alphabet[position + shift_amount]
 
-    print(f"The {direction}d text is :'{result_text}'")
+        print(f"The {direction}d text is :'{result_text}'")
 
-caesar(plain_text=text, shift_amount=shift, direction=direction)
+    caesar(plain_text=text, shift_amount=shift, direction=direction)
+    replay = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+
+    if replay == 'no':
+        end_of_game = True
