b659e26d138681aba9986756f02daea9e4498222
kakaa2993@ <kakaa2993@gmail.com>
2022-07-21 10:49:46 -0700

    add decrypt functionality to the code

--- Caesar_Cipher/main.py
+++ Caesar_Cipher/main.py
@@ -2,7 +2,7 @@
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']#, #" "]
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
@@ -15,19 +15,32 @@ def encrypt(plain_text, shift_amount):
     #cipher_text = "mjqqt"
     #print output: "The encoded text is mjqqt"
     cipher_text = ""
-
-    #if direction.lower().strip() == "encode":
     for letter in plain_text:
-        if alphabet.index(letter)+ shift_amount > len_alpha-1:
-            cipher_text += alphabet[(alphabet.index(letter) + shift) - len_alpha]
+        position = alphabet.index(letter)
+        new_position = (position + shift) - len_alpha
+        if position + shift_amount > len_alpha-1:
+            cipher_text += alphabet[new_position]
 
         else:
-            cipher_text += alphabet[alphabet.index(letter) + shift]
-    # else:
-    #     print("Go away")
-
+            cipher_text += alphabet[position + shift]
     print(f"The encode text is :'{cipher_text}'")
+
+def decrypt(cipher_text,shift_amount):
+    decrypt_text = ""
+
+    for letter in cipher_text:
+        position = alphabet.index(letter) 
+        new_position = position- shift_amount
+        decrypt_text += alphabet[new_position]
+
+    print(f"The decode text is :'{decrypt_text}'")
     ##���Bug alert: What happens if you try to encode the word 'civilization'?���
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt(plain_text=text, shift_amount=shift)
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction =="decode":
+    decrypt(cipher_text=text,shift_amount=shift)
+else:
+    print("You can only type (encode) for encrypting text and (decode) for decoding text")
