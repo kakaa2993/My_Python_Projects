0d42dbe125aee4f8e06ed7e6ac821034b24db4dd
kakaa2993@ <kakaa2993@gmail.com>
2022-07-21 06:39:01 -0700

    update the display list

--- Hungman_game/hungman.py
+++ Hungman_game/hungman.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python3
 
-#Step 2
+#Step 3
 
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
 
 guess = input("Guess a letter: ").lower()
-display = ["_" for letter in chosen_word]
-if geuss in chosen_word:
-    display[chosen_word.index(guess)] = 
-print(display)
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for letter in chosen_word:
+
+#Check guessed letter
+for position in range(word_length):
+    letter = chosen_word[position]
+    print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
-  
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+        display[position] = letter
 
-
+print(display)
 
 
 
