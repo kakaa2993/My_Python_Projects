c457cc3ccc161e62ac3d505acaec74e954dd06db
kakaa2993@ <kakaa2993@gmail.com>
2022-07-22 11:01:22 -0700

    add the bid functionality

--- Blind_Auction/main.py
+++ Blind_Auction/main.py
@@ -1,11 +1,44 @@
 #!/usr/bin/python3
 
-from art import logo
+from art import logo_
 import os
 
 #Print the logo and clear the screen
-print(logo)
 os.system("clear")
+print(logo_)
+
+
+end_of_bid = False
+bidders_dictionary = {}
+while not end_of_bid:
+    bidder_name = input("What is your name? ")
+    bid_amount = int(input("What is your bid? $"))
+    bidders_dictionary[bidder_name] = bid_amount
+    
+    # ask if the is another bidder
+    other_bidders = input("Are there any other bidders? 'yes' or 'no'.\n").lower()
+    if other_bidders == 'no':
+        winner_bidder = ""
+        top_bid_amount = 0
+        for bidder in bidders_dictionary:
+            bidder_bid = bidders_dictionary[bidder]
+            if bidder_bid >= top_bid_amount:
+                top_bid_amount = bidder_bid
+                winner_bidder = bidder
+        print()
+        print(f"The winner is '{winner_bidder}' with a bid of ${top_bid_amount}.")
+        print("Have a nice day!")
+        end_of_bid = True
+    else:
+        os.system("clear")
+    
+            
+
+
+
+
+
+#def find_the_winner(bidder_name, bid_amount):
 
 
 
