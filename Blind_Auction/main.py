f04871bd8b217e242b41ce195745f846a3f85517
kakaa2993@ <kakaa2993@gmail.com>
2022-07-22 11:55:56 -0700

    organize the code and delete useless comments

--- Blind_Auction/main.py
+++ Blind_Auction/main.py
@@ -1,33 +1,39 @@
 #!/usr/bin/python3
 
-from art import logo_
+from art import logo
 import os
 
 #Print the logo and clear the screen
 os.system("clear")
-print(logo_)
+print(logo)
 
 
 end_of_bid = False
 bidders_dictionary = {}
+
+def find_the_hghiest_bidder(bidders):
+    winner_bidder = ""
+    top_bid_amount = 0
+    for bidder in bidders:
+        bidder_bid = bidders[bidder]
+        if bidder_bid >= top_bid_amount:
+            top_bid_amount = bidder_bid
+            winner_bidder = bidder
+    print()
+    print(f"The winner is '{winner_bidder}' with a bid of '${top_bid_amount}'.")
+    print("Have a nice day!")
+
+
 while not end_of_bid:
-    bidder_name = input("What is your name? ")
-    bid_amount = int(input("What is your bid? $"))
+    bidder_name = input("What is your name?: ")
+    bid_amount = int(input("What is your bid?: $"))
     bidders_dictionary[bidder_name] = bid_amount
     
     # ask if the is another bidder
-    other_bidders = input("Are there any other bidders? 'yes' or 'no'.\n").lower()
+    other_bidders = input("Are there any other bidders? type 'yes' or 'no'.\n").lower()
     if other_bidders == 'no':
-        winner_bidder = ""
-        top_bid_amount = 0
-        for bidder in bidders_dictionary:
-            bidder_bid = bidders_dictionary[bidder]
-            if bidder_bid >= top_bid_amount:
-                top_bid_amount = bidder_bid
-                winner_bidder = bidder
-        print()
-        print(f"The winner is '{winner_bidder}' with a bid of ${top_bid_amount}.")
-        print("Have a nice day!")
+        find_the_hghiest_bidder(bidders_dictionary)
+
         end_of_bid = True
     else:
         os.system("clear")
