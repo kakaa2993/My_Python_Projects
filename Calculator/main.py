95c43f98a1227c1abd42054a222813e999597ca3
kakaa2993@ <kakaa2993@gmail.com>
2022-07-23 01:13:06 -0700

    add the continue functionality

--- Calculator/main.py
+++ Calculator/main.py
@@ -37,7 +37,33 @@ operation_symbol = input('Pick an operation symbol from the line above: ')
 num2 = int(input("What's the second number?: "))
 
 calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+answer = round(calculation_function(num1, num2),2)
 
 
 print(f" {num1} {operation_symbol} {num2} = {answer}")
+
+
+def replay(result):
+    player_want_to_continue = True
+    replay = input("do you want to continue? type 'y' to continue and type 'n' to exit: ").lower()
+
+    if replay == 'n':
+        player_want_to_continue = False
+        return 
+    while player_want_to_continue:
+        
+
+        operation_symbol = input("Pick an operation symbol ('+'','-','*','/') : ")
+        num3 = int(input("What's the next number?: "))
+
+        calculation_function = operations[operation_symbol]
+        result = calculation_function(result, num3)
+
+        print(f" {result} {operation_symbol} {num3} = {result}")
+        replay = input("do you want to continue? type 'y' to continue and type 'n' to exit: ").lower()
+
+        if replay == 'n':
+            player_want_to_continue = False
+    #replay(final_result)
+
+replay(answer)
