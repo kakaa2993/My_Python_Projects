8b101f7cd2fa648262ca77f1280b0355148c2569
kakaa2993@ <kakaa2993@gmail.com>
2022-07-22 23:17:14 -0700

    add the add, multipy, subtrac and divide functons

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 
+
+#Calculator
+
+# addition
+def add(num1, num2):
+    return num1 + num2
+
+# Subtract
+def subtract(num1, num2)
+    return num1 - num2
+
+# Multiply
+def multiply(num1, num2)
+    return num1 * num2
+
+# Divide
+def devide(num1, num2)
+    return num1 / num2
