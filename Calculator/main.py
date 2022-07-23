d9a5048f0bd6b7e50de3b6b8bc79994a10594498
kakaa2993@ <kakaa2993@gmail.com>
2022-07-23 00:00:34 -0700

    organize the code

--- Calculator/main.py
+++ Calculator/main.py
@@ -3,8 +3,8 @@
 
 #Calculator
 
-#print(operations)
 # addition
+
 def add(num1, num2):
     return num1 + num2
 
@@ -21,22 +21,23 @@ def divide(num1, num2):
     return num1 / num2
 
 operations = {
-	'+':add,
-	'-':subtract,
-	'*':multiply,
-	'/':divide,
+	'+': add,
+	'-': subtract,
+	'*': multiply,
+	'/': divide,
 }
 
 num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
 
 for symbol in operations:
     print(symbol)
 
 operation_symbol = input('Pick an operation symbol from the line above: ')
 
-function = operations[operation_symbol]
-answer = function(num1, num2)
+num2 = int(input("What's the second number?: "))
+
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
 
 
 print(f" {num1} {operation_symbol} {num2} = {answer}")
