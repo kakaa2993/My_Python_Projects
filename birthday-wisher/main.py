aac512147b5796a4b81d7a7c33b9c02535752364
kakaa2993@ <kakaa2993@gmail.com>
2022-09-10 17:34:59 +0100

    add all the functionality to the app

--- birthday-wisher/main.py
+++ birthday-wisher/main.py
@@ -1,35 +1,37 @@
-##################### Starting Project ######################
-
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter
-# templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
+#!/usr/bin/env python3
 import smtplib
 import random
 import datetime as dt
 import pandas
-now = dt.datetime.now()
-data = pandas.read_csv("birthdays.csv")
+
+
 my_email = ""
 my_password = ""
-letter_templates_list = ["letter_1.txt", "letter_2.txt","letter_3.txt"]
-print(data)
+letter_templates_list = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
+
+# compare the current day and the current month with the birthdays of the people in the birthdays.csv file
+now = dt.datetime.now()
+data = pandas.read_csv("birthdays.csv")
+
 for index, is_birthday_person in data.iterrows():
     if is_birthday_person.day == now.day and is_birthday_person.month == now.month:
-        recipe = is_birthday_person.email
-        # with open(random.choice(letter_templates_list), "r") as letter_file:
-        #     letter_file.readlines()
+
+        # create the letter tha will send
+        recipient_email = is_birthday_person.email
+        recipient_name = is_birthday_person["name"]
+        with open(f"letter_templates/{random.choice(letter_templates_list)}", "r") as letter_file:
+            letter_list = letter_file.readlines()
+            letter_list[0] = letter_list[0][0:letter_list[0].index("[")] + recipient_name + ",\n"
+        letter = "".join(letter_list)
+
+        # Send letter to that person
         with smtplib.SMTP("smtp.gmail.com") as connection:
             connection.starttls()
             connection.login(user=my_email, password=my_password)
             connection.sendmail(
                 from_addr=my_email,
-                to_addrs=recipe,
-                msg="Subject:Happy birthday!\n\n{}"
+                to_addrs=recipient_email,
+                msg=f"Subject:Happy birthday!\n\n{letter}"
             )
 
 
